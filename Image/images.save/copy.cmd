df9c361a66c7226a2c1306ceeacd98855c6f0727
Mike <mikecommon@gmail.com>
2025-05-25 13:18:29 -0400

    Copy command from archive images die to a working one

---
+++ Image/images.save/copy.cmd
@@ -0,0 +1,4 @@
+copy F:\Home\Image\images.save\driveway-2025-05-24-10-04-00.jpg  F:\Home\Image\images/driveway-2025-05-24-10-04-00.jpg 
+copy F:\Home\Image\images.save\driveway-2025-05-24-10-04-10.jpg  F:\Home\Image\images/driveway-2025-05-24-10-04-10.jpg 
+copy F:\Home\Image\images.save\driveway-2025-05-24-10-04-20.jpg  F:\Home\Image\images/driveway-2025-05-24-10-04-20.jpg 
+copy F:\Home\Image\images.save\driveway-2025-05-24-10-04-30.jpg  F:\Home\Image\images/driveway-2025-05-24-10-04-30.jpg 
